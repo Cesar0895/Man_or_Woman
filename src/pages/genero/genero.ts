83ffc2acd2e3c241a64964ccec4ad7d21c291ed9
Cesar Ramses Espino Serrano <ceraespinose@ittepic.edu.mx>
2018-04-11 16:29:17 -0600

    Done

---
+++ src/pages/genero/genero.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { CurpService } from "../../services/service";
+
+/**
+ * Generated class for the GeneroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-genero',
+  templateUrl: 'genero.html',
+})
+export class GeneroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public curpSrv: CurpService) {
+    console.log(`Curp: ${this.curpSrv.curp}`);
+    this.curpSrv.letra="H"==this.curpSrv.curp.substring(10,11);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GeneroPage');
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GeneroPage } from '../genero/genero';
+import { CurpService } from '../../services/service';
+import { compareDates } from 'ionic-angular/util/datetime-util';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public data:any={ 
+    curp:"",
+    letra:true
+}
 
+  constructor(public navCtrl: NavController, public curpSrv:CurpService) {
+    this.curpSrv.curp="";
+    
+    this.curpSrv.letra=true;
+    
+  }
+  Adivina() {
+      this.curpSrv.curp=this.data.curp;
+      console.log("curp"+this.data.curp);
+      this.navCtrl.push(GeneroPage);
   }
 
 }
